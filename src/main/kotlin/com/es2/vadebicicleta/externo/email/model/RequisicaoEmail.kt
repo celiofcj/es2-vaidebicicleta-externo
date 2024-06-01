0e461de79f04e08099f96dfe39ee33ea9cb2d0e6
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-06-01 04:41:41 -0300

    Adicionado equals e hashCode

--- src/main/kotlin/com/es2/vadebicicleta/externo/email/model/RequisicaoEmail.kt
+++ src/main/kotlin/com/es2/vadebicicleta/externo/email/model/RequisicaoEmail.kt
@@ -16,4 +16,26 @@ class RequisicaoEmail (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-)
+
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RequisicaoEmail
+
+        if (email != other.email) return false
+        if (assunto != other.assunto) return false
+        if (mensagem != other.mensagem) return false
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        var result = email.hashCode()
+        result = 31 * result + assunto.hashCode()
+        result = 31 * result + mensagem.hashCode()
+        result = 31 * result + (id?.hashCode() ?: 0)
+        return result
+    }
+}
---
+++ src/test/kotlin/com/es2/vadebicicleta/externo/email/EmailServiceTest.kt
@@ -0,0 +1,26 @@
+package com.es2.vadebicicleta.externo.email
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class EmailServiceTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun enviarEmail() {
+    }
+
+    @Test
+    fun validarFormatoEmail() {
+    }
+}
