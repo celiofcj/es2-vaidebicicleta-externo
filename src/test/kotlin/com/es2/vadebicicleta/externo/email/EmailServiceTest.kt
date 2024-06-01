d7198a1b4649270fff64ab3411edb926a4f2a14a
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-06-01 04:45:11 -0300

    WIP EmailService testes

--- src/test/kotlin/com/es2/vadebicicleta/externo/email/EmailServiceTest.kt
+++ src/test/kotlin/com/es2/vadebicicleta/externo/email/EmailServiceTest.kt
@@ -1,23 +1,51 @@
 package com.es2.vadebicicleta.externo.email
 
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import com.es2.vadebicicleta.externo.email.client.EmailClient
+import com.es2.vadebicicleta.externo.email.model.RequisicaoEmail
+import com.es2.vadebicicleta.externo.email.repository.EmailRepository
+import com.es2.vadebicicleta.externo.email.service.EmailService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.assertNotNull
 
-import org.junit.jupiter.api.Assertions.*
-
+@ExtendWith(MockKExtension::class)
 class EmailServiceTest {
-
-    @BeforeEach
-    fun setUp() {
-    }
-
-    @AfterEach
-    fun tearDown() {
-    }
+    @MockK
+    lateinit var emailRepositoryMock: EmailRepository
+    @MockK
+    lateinit var emailClientMock: EmailClient
+    @InjectMockKs
+    lateinit var emailService : EmailService
 
     @Test
-    fun enviarEmail() {
+    @DisplayName("Quando envio uma requisição com email válido, envia o email e persiste a requisição")
+    fun enviarEmailTeste() {
+        val email = "email@email.com"
+        val assunto = "Assunto do email"
+        val mensagem = "Mensagem do email"
+        val id = 47L
+        val requisicao = RequisicaoEmail(email,
+            assunto, mensagem)
+
+        every { emailClientMock.enviarEmail(email, assunto, mensagem) } returns Unit
+        every { emailRepositoryMock.save(requisicao) } returns RequisicaoEmail(email, assunto, mensagem, id)
+
+        val retorno = emailService.enviarEmail(RequisicaoEmail(email, assunto, mensagem))
+        assertNotNull(retorno, "Retorno não é nulo")
+
+        assertAll("Verificações do retorno",
+            { assertEquals(retorno.email, email, "Endereço de email retornado deve ser igual ao informado na requisição") },
+            { assertEquals(retorno.assunto, assunto, "Assunto retornado deve ser igual ao informado na requisição") },
+            { assertEquals(retorno.mensagem, mensagem, "Mensagem retornado deve ser igual ao informado na requisição") },
+            { assertEquals(retorno.id, id, "Id retornado deve ser igual ao id esperado (${id})") }
+        )
     }
 
     @Test
