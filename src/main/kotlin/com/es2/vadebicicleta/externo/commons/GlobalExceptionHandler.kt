b686327e300f29c6008306e2bd35a577c2b3c946
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-06-01 21:39:00 -0300

    Adicionando tratamento de exceção para erro de validação

--- src/main/kotlin/com/es2/vadebicicleta/externo/commons/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/es2/vadebicicleta/externo/commons/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.es2.vadebicicleta.externo.commons
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -11,6 +12,21 @@ class GlobalExceptionHandler {
     fun handleAllUncaughtExceptions(ex: Exception) : ResponseEntity<MensagemErro>{
         val mensagemErro =  MensagemErro("100", "Um erro inesperado aconteceu")
         return ResponseEntity.internalServerError().body(mensagemErro)
+    }
+
+    @ExceptionHandler
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException) : ResponseEntity<Collection<MensagemErro>>{
+        val bindingResult = ex.bindingResult
+        val fieldErrors = bindingResult.fieldErrors
+
+        val mensagensDeErro = fieldErrors.map {
+            val campo = it.field
+            val mensagem = it.defaultMessage
+            val codigo = "422"
+            MensagemErro(codigo, "$campo: $mensagem")
+        }
+
+        return ResponseEntity.unprocessableEntity().body(mensagensDeErro)
     }
 }
 
