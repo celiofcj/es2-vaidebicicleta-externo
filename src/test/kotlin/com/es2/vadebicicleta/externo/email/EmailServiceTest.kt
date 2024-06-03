a657d8b249c973c9a751f7426f5cb81f01a1959b
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-06-02 23:06:03 -0300

    Testes de enviar email expandidos

--- src/test/kotlin/com/es2/vadebicicleta/externo/email/EmailServiceTest.kt
+++ src/test/kotlin/com/es2/vadebicicleta/externo/email/EmailServiceTest.kt
@@ -3,7 +3,9 @@ package com.es2.vadebicicleta.externo.email
 import com.es2.vadebicicleta.externo.email.client.EmailClient
 import com.es2.vadebicicleta.externo.email.model.RequisicaoEmail
 import com.es2.vadebicicleta.externo.email.repository.EmailRepository
+import com.es2.vadebicicleta.externo.email.service.CouldNotSendEmailException
 import com.es2.vadebicicleta.externo.email.service.EmailService
+import com.es2.vadebicicleta.externo.email.service.WrongEmailAdressFormatException
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
@@ -11,10 +13,12 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import io.mockk.verifyAll
 import io.mockk.verifyOrder
+import org.junit.experimental.theories.suppliers.TestedOn
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import kotlin.test.assertNotNull
 
@@ -29,7 +33,7 @@ class EmailServiceTest {
 
     @Test
     @DisplayName("Quando envio uma requisição informações válidas, envia o email e persiste a requisição")
-    fun enviarEmailTesteSucesso() {
+    fun testeEnviarEmailSucesso() {
         val email = "email@email.com"
         val assunto = "Assunto do email"
         val mensagem = "Mensagem do email"
@@ -56,5 +60,31 @@ class EmailServiceTest {
             { assertEquals(retorno.mensagem, mensagem, "Mensagem retornado deve ser igual ao informado na requisição") },
             { assertEquals(retorno.id, id, "Id retornado deve ser igual ao id esperado (${id})") }
         )
+    }
+
+    @Test
+    @DisplayName("Quando envio endereço de email invalido, lança exceção")
+    fun testeEnviarEmailEnderecoInvalido() {
+        val email = "email.com"
+        val assunto = "Assunto do email"
+        val mensagem = "Mensagem do email"
+
+        assertThrows<WrongEmailAdressFormatException> ("A exceção deve ser lançada")
+        { emailService.enviarEmail(RequisicaoEmail(email, assunto, mensagem)) }
+    }
+
+    @Test
+    @DisplayName("Quando o envio do email falha, lança exceção")
+    fun enviarEmailFalhaNoEnvio() {
+        val email = "email@email.com"
+        val assunto = "Assunto do email"
+        val mensagem = "Mensagem do email"
+
+        every { emailClientMock.enviarEmail(email, assunto, mensagem) } throws Exception("Exceção genérica")
+
+        assertThrows<CouldNotSendEmailException> ("A exceção deve ser lançada")
+        { emailService.enviarEmail(RequisicaoEmail(email, assunto, mensagem)) }
+
+        verify { emailClientMock.enviarEmail(email, assunto, mensagem) }
     }
 }
