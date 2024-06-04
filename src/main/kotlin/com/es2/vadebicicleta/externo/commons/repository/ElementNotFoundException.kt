0780416f89f6b48bc9f6c8998e8d3a181f86140d
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-06-03 23:06:34 -0300

    Removido estrutura de repositório sem persistência

--- src/main/kotlin/com/es2/vadebicicleta/externo/commons/repository/ElementNotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package com.es2.vadebicicleta.externo.commons.repository
-
-class ElementNotFoundException : RuntimeException {
-    constructor() : super()
-    constructor(s: String?) : super(s)
-}
--- src/main/kotlin/com/es2/vadebicicleta/externo/commons/repository/Identificable.kt
+++
@@ -1,5 +0,0 @@
-package com.es2.vadebicicleta.externo.commons.repository
-
-interface Identificable<T> {
-    var id: T
-}
