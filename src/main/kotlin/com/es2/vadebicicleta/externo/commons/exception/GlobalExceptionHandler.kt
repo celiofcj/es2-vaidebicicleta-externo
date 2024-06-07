236cb4c348696e2bf33fd39fbf3d8f44ccbce893
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-06-06 22:48:42 -0300

    Adicionado logger

--- src/main/kotlin/com/es2/vadebicicleta/externo/commons/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/es2/vadebicicleta/externo/commons/exception/GlobalExceptionHandler.kt
@@ -2,10 +2,13 @@ package com.es2.vadebicicleta.externo.commons.exception
 
 import com.es2.vadebicicleta.externo.email.service.CouldNotSendEmailException
 import com.es2.vadebicicleta.externo.email.service.WrongEmailAdressFormatException
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+
+private val logger = KotlinLogging.logger {}
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -13,6 +16,7 @@ class GlobalExceptionHandler {
     @ExceptionHandler
     fun handleAllUncaughtExceptions(ex: Exception) : ResponseEntity<MensagemErro>{
         val mensagemErro =  MensagemErro("100", "Um erro inesperado aconteceu")
+        logger.error(ex) { ex.message }
         return ResponseEntity.internalServerError().body(mensagemErro)
     }
 
@@ -28,6 +32,7 @@ class GlobalExceptionHandler {
             MensagemErro(codigo, "$campo: $mensagem")
         }
 
+        logger.info (ex) { ex.message }
         return ResponseEntity.unprocessableEntity().body(mensagensDeErro)
     }
 
@@ -37,6 +42,7 @@ class GlobalExceptionHandler {
         val mensagem = ex.message ?: ""
         val mensagensDeErro = listOf<MensagemErro>(MensagemErro(codigo, mensagem))
 
+        logger.info (ex) { ex.message }
         return ResponseEntity.unprocessableEntity().body(mensagensDeErro)
     }
 
@@ -46,6 +52,7 @@ class GlobalExceptionHandler {
         val mensagem = ex.message ?: ""
         val mensagemErro = MensagemErro(codigo, mensagem)
 
+        logger.error(ex) { ex.message }
         return ResponseEntity.internalServerError().body(mensagemErro)
     }
 }
