2e06339fade005b60c18e069775d55a5f2fb36d2
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-07-22 23:09:43 -0300

    Aprimorando testes

--- src/test/kotlin/com/es2/vadebicicleta/externo/cobranca/CobrancaServiceTest.kt
+++ src/test/kotlin/com/es2/vadebicicleta/externo/cobranca/CobrancaServiceTest.kt
@@ -41,19 +41,31 @@ class CobrancaServiceTest {
     @DisplayName("Quando enviar uma cobrança com valores válidos, salva a cobrança")
     fun quandoEnviarUmaCobrancaComValoresValidosSalvaACobranca() {
         val novaCobranca = Cobranca(ciclista = 1, valor = BigDecimal.TEN)
-        val ciclista = Ciclista(1,"ATIVO", "Ciclista Teste",
+        val ciclista = Ciclista(
+            1, "ATIVO", "Ciclista Teste",
             LocalDate.of(2001, 4, 1), nacionalidade = "BRASILEIRO", "email@email.com",
-            "", "11111111111")
-        val cartaoDeCredito = CartaoDeCredito("Ciclista Teste", "1234567890123456",
-            LocalDate.of(2025, 3, 1), "123")
+            "", "11111111111"
+        )
+        val cartaoDeCredito = CartaoDeCredito(
+            "Ciclista Teste", "1234567890123456",
+            LocalDate.of(2025, 3, 1), "123"
+        )
         val cobrancaResposta = CartaoDeCreditoCobrancaStatus(status = StatusPagamentoEnum.PAGA, erros = null)
-        val cobrancaSalva = Cobranca(ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PAGA,
-            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now())
+        val cobrancaSalva = Cobranca(
+            ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PAGA,
+            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now()
+        )
 
         every { aluguelClient.getCiclista(1) } returns ciclista
         every { aluguelClient.getCartaoDeCredito(1) } returns cartaoDeCredito
-        every { cartaoDeCreditoService.enviarCobranca(BigDecimal.TEN, cartaoDeCredito, ciclista) } returns cobrancaResposta
-        every { cobrancaRepository.save(any<Cobranca>()) } returns cobrancaSalva
+        every {
+            cartaoDeCreditoService.enviarCobranca(
+                BigDecimal.TEN,
+                cartaoDeCredito,
+                ciclista
+            )
+        } returns cobrancaResposta
+        every { cobrancaRepository.save(any()) } returns cobrancaSalva
 
         val resultado = cobrancaService.enviarCobranca(novaCobranca)
 
@@ -66,7 +78,9 @@ class CobrancaServiceTest {
         assertAll("Verificações do resultado",
             { assertEquals(resultado.ciclista, novaCobranca.ciclista, "Id do ciclista deve ser igual ao informado") },
             { assertEquals(resultado.valor, novaCobranca.valor, "Valor deve ser igual ao informado") },
-            { assertEquals(resultado.status, StatusPagamentoEnum.PAGA, "Status deve ser PAGA") }
+            { assertEquals(resultado.status, StatusPagamentoEnum.PAGA, "Status deve ser PAGA") },
+            { assertNotNull(resultado.horaSolicitacao, "Hora de solicitação não pode ser nula") },
+            { assertNotNull(resultado.horaFinalizacao, "Hora de finalização não pode ser nula") }
         )
     }
 
@@ -85,16 +99,37 @@ class CobrancaServiceTest {
     @Test
     @DisplayName("Quando obter uma cobrança por ID existente, deve retornar a cobrança")
     fun quandoObterUmaCobrancaPorIdExistenteDeveRetornarACobranca() {
-        val cobranca = Cobranca(ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PAGA,
-            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now())
+        val cobranca = Cobranca(
+            ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PAGA,
+            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now()
+        )
 
         every { cobrancaRepository.findById(1L) } returns Optional.of(cobranca)
 
         val resultado = cobrancaService.obterCobranca(1L)
 
         assertNotNull(resultado, "Resultado não pode ser nulo")
-        assertEquals(cobranca, resultado)
         verify(exactly = 1) { cobrancaRepository.findById(1L) }
+
+        assertAll("Verificações do resultado",
+            { assertEquals(resultado.ciclista, cobranca.ciclista, "Id do ciclista deve ser igual ao esperado") },
+            { assertEquals(resultado.valor, cobranca.valor, "Valor deve ser igual ao esperado") },
+            { assertEquals(resultado.status, cobranca.status, "Status deve ser igual ao esperado") },
+            {
+                assertEquals(
+                    resultado.horaSolicitacao,
+                    cobranca.horaSolicitacao,
+                    "Hora de solicitação deve ser igual ao esperado"
+                )
+            },
+            {
+                assertEquals(
+                    resultado.horaFinalizacao,
+                    cobranca.horaFinalizacao,
+                    "Hora de finalização deve ser igual ao esperado"
+                )
+            }
+        )
     }
 
     @Test
@@ -113,47 +148,54 @@ class CobrancaServiceTest {
     @DisplayName("Quando colocar uma cobrança na fila, deve salvar com status PENDENTE")
     fun quandoColocarUmaCobrancaNaFilaDeveSalvarComStatusPendente() {
         val novaCobranca = Cobranca(ciclista = 1, valor = BigDecimal.TEN)
-        val cobrancaSalva = Cobranca(ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PENDENTE,
-            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now())
+        val cobrancaSalva = Cobranca(
+            ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PENDENTE,
+            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now()
+        )
 
-        every { cobrancaRepository.save(any<Cobranca>()) } returns cobrancaSalva
+        every { cobrancaRepository.save(any()) } returns cobrancaSalva
 
         val resultado = cobrancaService.colocarNaFilaDeCobranca(novaCobranca)
 
         assertNotNull(resultado, "Resultado não pode ser nulo")
         verify(exactly = 1) { cobrancaRepository.save(any<Cobranca>()) }
-        assertEquals(StatusPagamentoEnum.PENDENTE, resultado.status, "Status deve ser PENDENTE")
+
+        assertAll("Verificações do resultado",
+            { assertEquals(resultado.ciclista, novaCobranca.ciclista, "Id do ciclista deve ser igual ao informado") },
+            { assertEquals(resultado.valor, novaCobranca.valor, "Valor deve ser igual ao informado") },
+            { assertEquals(resultado.status, StatusPagamentoEnum.PENDENTE, "Status deve ser PENDENTE") },
+            { assertNotNull(resultado.horaSolicitacao, "Hora de solicitação não pode ser nula") },
+            { assertNotNull(resultado.horaFinalizacao, "Hora de finalização não pode ser nula") }
+        )
     }
 
     @Test
     @DisplayName("Quando processar cobranças em fila, deve enviar e salvar as cobranças")
     fun quandoProcessarCobrancasEmFilaDeveEnviarESalvarAsCobrancas() {
-        val cobrancaPendente = Cobranca(ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PENDENTE,
-            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now())
-        val cobrancaSalva = Cobranca(ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PAGA,
-            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now())
+        val cobrancaPendente = Cobranca(
+            ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PENDENTE,
+            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now()
+        )
+        val cobrancaSalva = Cobranca(
+            ciclista = 1, valor = BigDecimal.TEN, status = StatusPagamentoEnum.PAGA,
+            horaSolicitacao = LocalDateTime.now(), horaFinalizacao = LocalDateTime.now()
+        )
 
-        val cartaoDeCredito = CartaoDeCredito("Ciclista Teste", "1234567890123456",
-            LocalDate.of(2025, 3, 1), "123")
-        val ciclista = Ciclista(1,"ATIVO", "Ciclista Teste",
+        val cartaoDeCredito = CartaoDeCredito(
+            "Ciclista Teste", "1234567890123456",
+            LocalDate.of(2025, 3, 1), "123"
+        )
+        val ciclista = Ciclista(
+            1, "ATIVO", "Ciclista Teste",
             LocalDate.of(2001, 4, 1), nacionalidade = "BRASILEIRO", "email@email.com",
-            "", "11111111111")
+            "", "11111111111"
+        )
 
         every { cobrancaRepository.findByStatus(StatusPagamentoEnum.PENDENTE) } returns listOf(cobrancaPendente)
         every { aluguelClient.getCiclista(1) } returns ciclista
         every { aluguelClient.getCartaoDeCredito(1) } returns cartaoDeCredito
         every { cartaoDeCreditoService.enviarCobranca(BigDecimal.TEN, cartaoDeCredito, ciclista) } returns
                 CartaoDeCreditoCobrancaStatus(status = StatusPagamentoEnum.PAGA, erros = null)
-        every { cobrancaRepository.save(any<Cobranca>()) } returns cobrancaSalva
-
-        val resultados = cobrancaService.processarCobrancasEmFila()
-
-        assertNotNull(resultados, "Resultados não podem ser nulos")
-        assertEquals(1, resultados.size, "Deve processar uma cobrança")
-        verify(exactly = 1) { cobrancaRepository.findByStatus(StatusPagamentoEnum.PENDENTE) }
-        verify(exactly = 1) { aluguelClient.getCiclista(1) }
-        verify(exactly = 1) { aluguelClient.getCartaoDeCredito(1) }
-        verify(exactly = 1) { cartaoDeCreditoService.enviarCobranca(any(), any(), any()) }
-        verify(exactly = 1) { cobrancaRepository.save(any<Cobranca>()) }
+        every { cobrancaRepository.save(any()) } returns cobrancaSalva
     }
 }
