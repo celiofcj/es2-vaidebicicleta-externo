c37899eb6a3928d64e9b4902f194b91783fc82c6
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-08-13 14:27:48 -0300

    Revert "Removendo interceptor desnecessário"
    This reverts commit 7edad792f300b2de3366fa761ec7313e69a60d9c.

--- src/main/kotlin/com/es2/vadebicicleta/externo/commons/config/RestConfig.kt
+++ src/main/kotlin/com/es2/vadebicicleta/externo/commons/config/RestConfig.kt
@@ -3,6 +3,8 @@ package com.es2.vadebicicleta.externo.commons.config
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 import java.time.Duration
 
@@ -11,9 +13,15 @@ import java.time.Duration
 class RestConfig {
     @Bean
     fun configRestTemplate(builder: RestTemplateBuilder): RestTemplate {
+        val httpRequestFactory = { BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory()) }
         return builder
             .setConnectTimeout(Duration.ofSeconds(30))
             .setReadTimeout(Duration.ofSeconds(30))
+            .requestFactory(httpRequestFactory)
             .build()
+            .apply {
+                interceptors = mutableListOf()
+                interceptors.add(RestTemplateLogger())
+            }
     }
 }
---
+++ src/main/kotlin/com/es2/vadebicicleta/externo/commons/config/RestTemplateLogger.kt
@@ -0,0 +1,54 @@
+package com.es2.vadebicicleta.externo.commons.config
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
+
+private val logger = KotlinLogging.logger {}
+
+class RestTemplateLogger : ClientHttpRequestInterceptor {
+    @Throws(IOException::class)
+    override fun intercept(
+        request: HttpRequest,
+        body: ByteArray,
+        execution: ClientHttpRequestExecution,
+    ): ClientHttpResponse {
+        traceRequest(request, body)
+        val response = execution.execute(request, body)
+        traceResponse(response)
+        return response
+    }
+
+    @Throws(IOException::class)
+    private fun traceRequest(request: HttpRequest, body: ByteArray) {
+        logger.debug{"===========================request begin================================================"}
+        logger.debug{"URI         : ${request.uri}"}
+        logger.debug{"Method      : ${request.method}"}
+        logger.debug{"Headers     : ${request.headers}"}
+        logger.debug{"Request body: ${String(body, charset("UTF-8"))}"}
+        logger.debug{"==========================request end================================================"}
+    }
+
+    @Throws(IOException::class)
+    private fun traceResponse(response: ClientHttpResponse) {
+        val inputStringBuilder = StringBuilder()
+        val bufferedReader = BufferedReader(InputStreamReader(response.body, "UTF-8"))
+        var line = bufferedReader.readLine()
+        while (line != null) {
+            inputStringBuilder.append(line)
+            line = bufferedReader.readLine()
+        }
+        logger.debug{"============================response begin=========================================="}
+        logger.debug{"Status code  : ${response.statusCode}"}
+        logger.debug{"Status text  : ${response.statusText}"}
+        logger.debug{"Headers      : ${response.headers}"}
+        logger.debug{"Response body: $inputStringBuilder}"}
+        logger.debug{"=======================response end================================================="}
+    }
+}
