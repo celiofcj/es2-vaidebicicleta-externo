7edad792f300b2de3366fa761ec7313e69a60d9c
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-08-13 04:24:27 -0300

    Removendo interceptor desnecessário

--- src/main/kotlin/com/es2/vadebicicleta/externo/commons/config/RestConfig.kt
+++ src/main/kotlin/com/es2/vadebicicleta/externo/commons/config/RestConfig.kt
@@ -3,8 +3,6 @@ package com.es2.vadebicicleta.externo.commons.config
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.BufferingClientHttpRequestFactory
-import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 import java.time.Duration
 
@@ -13,15 +11,9 @@ import java.time.Duration
 class RestConfig {
     @Bean
     fun configRestTemplate(builder: RestTemplateBuilder): RestTemplate {
-        val httpRequestFactory = { BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory()) }
         return builder
             .setConnectTimeout(Duration.ofSeconds(30))
             .setReadTimeout(Duration.ofSeconds(30))
-            .requestFactory(httpRequestFactory)
             .build()
-            .apply {
-                interceptors = mutableListOf()
-                interceptors.add(RestTemplateLogger())
-            }
     }
 }
